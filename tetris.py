70d2feeb6c6e292e8288b18aa1014ff279b3985a
Natniif <fintan.hardy@gmail.com>
2023-10-19 21:55:06 +0100

    were halfway there oooooooo

--- tetris.py
+++ tetris.py
@@ -16,70 +16,83 @@ BLUE = (51, 153, 255)
 GREEN = (0, 204, 0)
 RED = (255, 0, 0)
 PURPLE = (153, 0, 153)
+BLACK = (0, 0, 0)
 
 def initialiseGrid(): 
-	return [[0 for _ in range(15)] for _ in range(10)]
+	return [[0 for _ in range(GRID_HEIGHT)] for _ in range(GRID_WIDTH)]
+
 
 
 class Shape(): 
 
 	pieceNum = 0
 
-	def __init__(self, pieceShape): 
+	def __init__(self, pieceShape: list[list]): 
 		self.piece = pieceShape
 
 		# x and y track position of shape on board
-		self.x = (GRID_WIDTH + self.piece) // 2
+		# both positions track the top left position of the piece on the board
+		self.x = (GRID_WIDTH - len(self.piece)) // 2
 		self.y = 0
 
+		self.isNew = True
+
 	def canMoveRight(self, grid):
-		for i in self.piece[0].length(): 
+		for i in len(self.piece[0]): 
 			if grid[(self.x + self.piece.length()) + 1][self.y + i] != 0:
 				self.x += 1
+				return True
 			else: 
 				return False
 
 	def canMoveLeft(self, grid): 
-		for i in self.piece[0].length(): 
+		for i in len(self.piece[0]): 
 			if grid[self.x - 1][self.y + i] != 0:
 				self.x -= 1
+				return True
 		else: 
 			return False
 
 	def canMoveDown(self, grid): 
-		for i in self.piece.length(): 
-			if grid[(self.x + i - 1)][self.y + self.piece[0].length()] != 0:
+		for i in range(len(self.piece)): 
+			if (self.x + i) >= GRID_WIDTH or (self.y + len(self.piece[0])) >= GRID_HEIGHT:
+				self.endPiece()
+				return False
+			elif grid[(self.x + i)][self.y + len(self.piece[0])] != 0:
+				self.endPiece()
 				return False
 
 		self.y += 1	
+		self.isNew = False
 		return True
 
 	def rotatePieceLeft(self, grid)-> Union[list[list], bool]: 
-		rows = self.piece.length()
-		cols = self.piece[0].length()
+		rows = len(self.piece)
+		cols = len(self.piece[0])
 
 		newpiece = [[0 for _ in range(cols)] for _ in range(rows)]
 
 		# TODO: this is probably wrong
-		for i in rows: 
-			if grid[self.x + i][self.y + j] != 0:
-				return False
+		for i in range(rows): 
+			for j in range(cols): 
+				if grid[self.x + i][self.y + j] != 0:
+					return False
 			# TODO: need to figure out how to test left direction as well 
 
-		for i in rows: 
-			for j in cols: 
+		for i in range(rows): 
+			for j in range(cols): 
 				newpiece[j][i] = self.piece[i][j]
 
 		return newpiece
 
 	def rotatePieceRight(self, grid)-> Union[list[list], bool]:
-		rows = self.piece.length()
-		cols = self.piece[0].length()
+		rows = len(self.piece)
+		cols = len(self.piece[0])
 
 		newpiece = [[0 for _ in range(cols)] for _ in range(rows)]
 
-		for i in rows: 
-			for j in cols: 
+		for i in range(rows): 
+			for j in range(cols): 
 				newpiece[j][i] = self.piece[rows - i][cols - j]
 
 		return newpiece
@@ -89,11 +102,14 @@ class Shape():
 		cls.pieceNum += 1
 
 	def endPiece(self): 
-		for i in self.piece.length():
-			for j in self.piece[0].length(): 
-				self.piece[i][j] == self.piece[i][j].lower()
+		for i in range(len(self.piece)):
+			for j in range(len(self.piece[0])): 
+				if (isinstance(self.piece[i][j], str)):
+					self.piece[i][j] == self.piece[i][j].lower()
 
 		self.incDeadNum()
+		# return true so that pointer can now point to new piece
+		return True
 
 	def step(self, grid): 
 		if self.canMoveDown(grid): 
@@ -103,32 +119,33 @@ class Shape():
 
 
 Tshape = Shape([
-    [0, 'R', 0],
-    ['R', 'R', 'R'],
+    [0, "R", 0],
+    ["R", "R", "R"],
 ])
 
 Lshape = Shape([
-    ['B', 0],
-    ['B', 0],
-    ['B', 'B'],
+    ["B", 0],
+    ["B", 0],
+    ["B", "B"],
 ])
 
-Ishape = Shape([['G'], ['G'], ['G']])
+Ishape = Shape([["G"], ["G"], ["G"]])
 
 SquareShape = Shape([
-    ['Y', 'Y'],
-    ['Y', 'Y'],
+    ["P", "P"],
+    ["P", "P"],
 ])
 
 colors = {
-	'R': "RED",
-	'G': "GREEN", 
-	'B': "BLUE", 
-	'P': "PURPLE",
-	'r': "RED",
-	'g': "GREEN", 
-	'b': "BLUE", 
-	'p': "PURPLE"
+	"R": RED,
+	"G": GREEN, 
+	"B": BLUE, 
+	"P": PURPLE,
+	"r": RED,
+	"g": GREEN, 
+	"b": BLUE, 
+	"p": PURPLE,
+	0 : BLACK, 
 }
 
 def randomPiece() -> Shape:
@@ -138,35 +155,41 @@ def randomPiece() -> Shape:
 	return shapes[randomIndex]
 
 def drawGrid(grid: list[list], screen, cell_size): 
-	for i in range(GRID_HEIGHT):
-		for j in range(GRID_WIDTH):
+	for i in range(GRID_WIDTH):
+		for j in range(GRID_HEIGHT):
 			color = colors[grid[i][j]] 
-			rect = pygame.Rect(j * CELL_SIZE, i * CELL_SIZE, 
+			rect = pygame.Rect(i * CELL_SIZE, j * CELL_SIZE, 
 					  CELL_SIZE, CELL_SIZE)
 			pygame.draw.rect(screen, color, rect)
 
 def updateGrid(grid: list[list], shape: Shape): 
-	pass
+	#set old falling piece position to black again
+	if (shape.isNew == False): 
+		for i in range(len(shape.piece)):
+			for j in range(len(shape.piece[0])):
+				grid[shape.x + i][shape.y - 1 + j] = 0 
 
+	for i in range(len(shape.piece)):
+		for j in range(len(shape.piece[0])):
+			grid[shape.x + i][shape.y + j] = shape.piece[i][j]
 
-def userInput(piece: Shape, event): 
+	return grid
+
+def userInput(grid: list[list], piece: Shape, event): 
 	if event.type == pygame.KEYDOWN:	
 		if event.key == pygame.K_LEFT:
-			piece.canMoveLeft
+			piece.canMoveLeft(grid)
 		elif event.key == pygame.K_RIGHT:
-			piece.canMoveRight
+			piece.canMoveRight(grid)
 		elif event.key == pygame.K_DOWN:
 			pass
 		elif event.key == pygame.K_SPACE:
 			# space bar rotates the piece
 			pass
 
-def step(shape: Shape, grid): 
-	pass
-
 def main(): 
 	# fixed FPS for now 
-	FPS = 10
+	FPS = 5
 	grid = initialiseGrid()
 
 	pygame.init() 
@@ -176,15 +199,24 @@ def main():
 	running = True
 	clock = pygame.time.Clock()
 
+	# init pointer to piece
+	currentPiece = randomPiece()
 	while running:
 		for event in pygame.event.get():
-			userInput(event)
+			if event.type is not None:
+				userInput(grid, currentPiece, event)
 			if event.type == pygame.QUIT:
 				running = False
 
-		drawGrid(grid, CELL_SIZE, grid, screen)
+		grid = updateGrid(grid, currentPiece)
+		drawGrid(grid, screen, CELL_SIZE)
 
-		step()
+		# if cant move down init new random piece and change pointer to new random piece
+		if (currentPiece.canMoveDown(grid) == False): 
+			currentPiece = randomPiece()
+		#else currentPiece.canMoveDown(grid):
+			
+	
 
 		pygame.display.flip()
 
